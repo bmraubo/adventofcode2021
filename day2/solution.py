7edd706247a06fa5680a5dc3af7f8b5d0833c4ef
bmraubo <bmraubo@gmail.com>
2021-12-02 12:38:54 +0000

    day 2 part1 solved

---
+++ day2/solution.py
@@ -0,0 +1,39 @@
+file = "day2/input.txt"
+
+
+def ReadInput(file):
+    def ConvertToTuple(instruction):
+        direction = instruction.split()[0]
+        value = instruction.split()[1]
+        return (direction, int(value))
+
+    instructions = []
+    with open(file, "r") as input:
+        input_string = input.read()
+        for instruction in input_string.split("\n"):
+            instructions.append(ConvertToTuple(instruction))
+    return instructions
+
+
+def CalculateLocation(instructions):
+    horizontal_position = 0
+    depth = 0
+
+    for direction, value in instructions:
+        if direction == "forward":
+            horizontal_position += value
+        elif direction == "down":
+            depth += value
+        elif direction == "up":
+            depth -= value
+    return (horizontal_position, depth)
+
+
+def Multiply(location):
+    return location[0] * location[1]
+
+
+if __name__ == "__main__":
+    instructions = ReadInput(file)
+    location = CalculateLocation(instructions)
+    print(Multiply(location))
