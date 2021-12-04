859deb12589d10967b11b3df4eff69ff03635009
bmraubo <bmraubo@gmail.com>
2021-12-04 12:21:47 +0000

    obtaining gamma rate

--- day3/solution.py
+++ day3/solution.py
@@ -20,3 +20,21 @@ def create_bit_columns(input_list):
         bit_columns[x] = create_column(x, input_list)
 
     return bit_columns
+
+
+def find_most_common(bit_columns):
+    def count_bits(column):
+        if column.count("1") > column.count("0"):
+            return "1"
+        elif column.count("0") > column.count("1"):
+            return "0"
+
+    gamma_list = []
+    for index, column in bit_columns.items():
+        gamma_list.append(count_bits(column))
+
+    return gamma_list
+
+
+def convert_list_to_string(input_list):
+    return "".join(input_list)
--- day3/solution_test.py
+++ day3/solution_test.py
@@ -5,7 +5,7 @@ import solution
 def test_read_input_file():
     test_file = "testinput.txt"
 
-    expected_result = ["1011", "0101", "1010"]
+    expected_result = ["101110", "010101", "101010"]
 
     assert solution.read_input_file(test_file) == expected_result
 
@@ -20,6 +20,26 @@ def test_create_bit_list():
         1: ["0", "1", "0"],
         2: ["1", "0", "1"],
         3: ["1", "1", "0"],
+        4: ["1", "0", "1"],
+        5: ["0", "1", "0"],
     }
 
     assert solution.create_bit_columns(input_list) == expected_result
+
+
+def test_find_most_common():
+    test_file = "testinput.txt"
+
+    input_list = solution.read_input_file(test_file)
+
+    bit_columns = solution.create_bit_columns(input_list)
+
+    expected_result = ["1", "0", "1", "1", "1", "0"]
+
+    assert solution.find_most_common(bit_columns) == expected_result
+
+
+def test_convert_list_to_string():
+    test_input = ["1", "0", "1", "1", "1", "0"]
+
+    assert solution.convert_list_to_string(test_input) == "101110"
