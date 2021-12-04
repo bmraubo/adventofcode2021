a2d5e33637d21e20bb0f4aaa5fc53383dad59fe8
bmraubo <bmraubo@gmail.com>
2021-12-04 11:43:47 +0000

    adjusted test input to make it more informative

--- day3/solution_test.py
+++ day3/solution_test.py
@@ -5,7 +5,7 @@ import solution
 def test_read_input_file():
     test_file = "testinput.txt"
 
-    expected_result = ["123", "456", "789"]
+    expected_result = ["1011", "0101", "1010"]
 
     assert solution.read_input_file(test_file) == expected_result
 
@@ -15,6 +15,11 @@ def test_create_bit_list():
 
     input_list = solution.read_input_file(test_file)
 
-    expected_result = {0: ["1", "4", "7"], 1: ["2", "5", "8"], 2: ["3", "6", "9"]}
+    expected_result = {
+        0: ["1", "0", "1"],
+        1: ["0", "1", "0"],
+        2: ["1", "0", "1"],
+        3: ["1", "1", "0"],
+    }
 
     assert solution.create_bit_columns(input_list) == expected_result
